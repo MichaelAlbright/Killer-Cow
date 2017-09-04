4e8ea82e1b200cfbba90ee04d6ce8d2dea412be3
MichaelAlbright <michaelalbright25@yahoo.com>
2017-09-04 15:24:26 -0400

    adding options to menu

--- Assets/Scripts/DialogueHolder.cs
+++ Assets/Scripts/DialogueHolder.cs
@@ -11,23 +11,23 @@ public class DialogueHolder : MonoBehaviour {
 
 	public GameObject button;
 
+	private bool inArea;
+
 	// Use this for initialization
 	void Start () {
 		dMan = FindObjectOfType<DialogueManager> ();
+
+		inArea = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-	void OnTriggerStay2D (Collider2D other)
-	{
-		if (other.gameObject.name == "Player") {
+		if (inArea) {
 			if (Input.GetButtonUp ("Jump")) {
-//				dMan.ShowBox (dialogue);
+				//				dMan.ShowBox (dialogue);
 
 				if (!dMan.dialogueActive) {
+					inArea = false;
 					dMan.dialogueLines = dialogueLines;
 					dMan.currentLine = 0;
 					dMan.ShowDialogue ();
@@ -40,10 +40,31 @@ public class DialogueHolder : MonoBehaviour {
 		}
 	}
 
+//	void OnTriggerStay2D (Collider2D other)
+//	{
+//		if (other.gameObject.name == "Player") {
+//			if (Input.GetButtonUp ("Jump")) {
+////				dMan.ShowBox (dialogue);
+//
+//				if (!dMan.dialogueActive) {
+//					inArea = false;
+//					dMan.dialogueLines = dialogueLines;
+//					dMan.currentLine = 0;
+//					dMan.ShowDialogue ();
+//				}
+//
+//				if (transform.parent.GetComponent<VillagerMovement> () != null) {
+//					transform.parent.GetComponent<VillagerMovement> ().canMove = false;
+//				}
+//			}
+//		}
+//	}
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		if (other.gameObject.name == "Player") {
 			button.SetActive (true);
+			inArea = true;
 		}
 	}
 
@@ -51,6 +72,7 @@ public class DialogueHolder : MonoBehaviour {
 	{
 		if (other.gameObject.name == "Player") {
 			button.SetActive (false);
+			inArea = false;
 		}
 	}
 }
